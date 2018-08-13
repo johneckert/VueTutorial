aa7e5e6aff657d5b8be3cb07a5ec8582431811e3
johneckert <johnteckert@gmail.com>
2018-08-13 10:02:23 -0400

    fixed tabs

--- communication-exercise/src/components/Server/Servers.vue
+++ communication-exercise/src/components/Server/Servers.vue
@@ -1,16 +1,16 @@
 <template>
-      <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="server in servers"
-                    :key="server.id"
-                    @click="selectServer(server)" 
-                    @changeStatus="server.status = $event">
-                        Server #{{ server.id }}
-            </li>
-        </ul>
-    </div>
+  <div class="col-xs-12 col-sm-6">
+    <ul class="list-group">
+      <li
+        class="list-group-item"
+        v-for="server in servers"
+        :key="server.id"
+        @click="selectServer(server)" 
+        @changeStatus="server.status = $event">
+            Server #{{ server.id }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
