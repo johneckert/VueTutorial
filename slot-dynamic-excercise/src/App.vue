71bd1af935efb7387863016147a927695369c94c
johneckert <johnteckert@gmail.com>
2018-08-13 10:58:48 -0400

    done

--- slot-dynamic-excercise/src/App.vue
+++ slot-dynamic-excercise/src/App.vue
@@ -3,30 +3,45 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button 
+                    class="btn btn-primary"
+                    @click="selectedComp = 'appBlue'">
+                        Load Blue Template
+                </button>
+                <button 
+                    class="btn btn-success"
+                    @click="selectedComp = 'appGreen'">
+                        Load Green Template
+                </button>
+                <button class="btn btn-danger"
+                @click="selectedComp = 'appRed'">
+                    Load Red Template
+                </button>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
-            </div>
+                <component :is="selectedComp">
+                    <h1 slot="title">Title</h1>
+                    <p slot="body">Body</p>
+                </component>
+            </div>      
         </div>
     </div>
 </template>
 
 <script>
-    import Blue from './components/Blue.vue';
-    import Green from './components/Green.vue';
-    import Red from './components/Red.vue';
+import Blue from "./components/Blue.vue";
+import Green from "./components/Green.vue";
+import Red from "./components/Red.vue";
 
-    export default {
-        components: {
-            appBlue: Blue,
-            appGreen: Green,
-            appRed: Red
-        }
-    }
+export default {
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
+  },
+  data: function() {
+    return { selectedComp: "appBlue" };
+  }
+};
 </script>
 
 <style>
