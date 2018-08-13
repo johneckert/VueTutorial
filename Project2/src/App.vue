5c7cd7827a87e1e684b442c347cf1966607bfe39
johneckert <johnteckert@gmail.com>
2018-08-13 14:06:53 -0400

    finished second project

--- Project2/src/App.vue
+++ Project2/src/App.vue
@@ -1,13 +1,45 @@
 <template>
     <div class="container">
-    
+        <quote-header :maxQuotes="maxQuotes" :quoteCount="quotes.length"></quote-header>
+        <new-quote @quoteAdded="addQuote"></new-quote>
+        <quote-grid :quotes="quotes" @quoteSelected="deleteQuote"></quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Click on a quote to delete it</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuotes.vue";
+import Header from "./components/Header.vue";
+export default {
+  data: function() {
+    return {
+      quotes: ["test quote"],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    quoteGrid: QuoteGrid,
+    newQuote: NewQuote,
+    quoteHeader: Header
+  },
+  methods: {
+    addQuote(quote) {
+      if (this.quotes.length < this.maxQuotes) {
+        this.quotes.push(quote);
+      } else {
+        alert("Please Delete Old Quotes First");
+      }
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
+  }
+};
 </script>
 
 <style>
---
+++ Project2/src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(quote, i) in quotes" :key="i" @click.native="selectQuote(i)">
+      {{quote}}
+    </app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  },
+  methods: {
+    selectQuote(index) {
+      this.$emit("quoteSelected", index);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
